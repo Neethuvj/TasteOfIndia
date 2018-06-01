3575111c1baf7751688dcc52fb421031a41e7a78
Neethu <neethuvj74@gmail.com>
2018-06-01 19:26:52 +1000

    single page applications part1

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -9,5 +9,6 @@ import { HOME } from '@angular/cdk';
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'menu',component: MenuComponent},
+    {path: 'contactus',component: ContactComponent},
     {path: '',redirectTo: '/home',pathMatch:'full'}
 ]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Dish} from '../shared/dish';
+import {DishService} from '../services/dish.service';
+
+import {Promotion} from '../shared/promotion';
+import {PromotionService} from '../services/promotion.service';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,7 +14,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  dish: Dish;
+  promotion: Promotion;
+
+  constructor(private dishService: DishService,private promotionService: PromotionService) {
+
+    this.dish = this.dishService.getFeaturedDish();
+    this.promotion = this.promotionService.getFeaturedPromotion();
+   }
 
   ngOnInit() {
   }
